ea515340d1668dadbf0315571e66dd31303b7ef4
s1ddok <siddok@gmail.com>
2017-12-18 18:33:36 +0300

    Initial Commit

---
+++ ARKitHitTestVisualizer/ARKitHitTestVisualizer.h
@@ -0,0 +1,19 @@
+//
+//  ARKitHitTestVisualizer.h
+//  ARKitHitTestVisualizer
+//
+//  Created by Andrey Volodin on 18.12.17.
+//  Copyright © 2017 Andrey Volodin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKitHitTestVisualizer.
+FOUNDATION_EXPORT double ARKitHitTestVisualizerVersionNumber;
+
+//! Project version string for ARKitHitTestVisualizer.
+FOUNDATION_EXPORT const unsigned char ARKitHitTestVisualizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKitHitTestVisualizer/PublicHeader.h>
+
+
